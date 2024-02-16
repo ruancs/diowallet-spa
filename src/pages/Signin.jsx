e9223e128fec17e9d4bfffe7e0838106e4fd3479
ruancs <ruan.cs@hotmail.com>
2024-02-16 01:07:39 -0300

    feat: react hook form

--- src/pages/Signin.jsx
+++ src/pages/Signin.jsx
@@ -2,18 +2,25 @@ import { Link } from 'react-router-dom'
 import logo from '../assets/diologo.png'
 import Button from '../components/Button'
 import Input from '../components/Input'
+import { useForm } from 'react-hook-form'
 
 export default function Signin() {
+
+    const {register, handleSubmit} = useForm();
+
+    function handleSubmitForm(data){
+        console.log(data)
+    }
     
     return(
         <div className="flex flex-col items-center justify-around bg-[#2E2E2E] shadow-sm  rounded p-8 w-[35rem] h-[35rem]">
             <img src={logo} className="w-44" alt='diowallet logo'/>
-            <form action="" className="flex flex-col justify-center gap-4 w-full text-2xl">
-                <Input type="email" placeholder="Email"/>
-                <Input type="password" placeholder="Password"/>
+            <form onSubmit={handleSubmit(handleSubmitForm)} action="" className="flex flex-col justify-center gap-4 w-full text-2xl">
+                <Input type="email" placeholder="Email" register={register} name="email"/>
+                <Input type="password" placeholder="Password" register={register} name="password"/>
                 <Button type="submit" text="SIGNIN"/>
             </form>
             <p className="text-white text-1xl">Dont have an account? <Link to="/signup" className="text-sky-400 hover:text-sky-600">Register</Link></p>
         </div>
-    )
+    );
 }
--- src/pages/Signup.jsx
+++ src/pages/Signup.jsx
@@ -3,8 +3,15 @@ import logo from '../assets/diologo.png'
 import Button from '../components/Button'
 import Input from '../components/Input'
 import { BiArrowBack } from "react-icons/bi";
+import { useForm } from 'react-hook-form';
 
 export default function Signup(){
+    const {register, handleSubmit} = useForm();
+
+    function handleSubmitForm(data){
+        console.log(data)
+    }
+
     return(
         <div className="flex flex-col items-center justify-around bg-[#2E2E2E] shadow-sm rounded p-8 w-[35rem] h-[35rem] relative">
             <Link to="/signin">
@@ -12,11 +19,11 @@ export default function Signup(){
             </Link>
             
             <img src={logo} className="w-44" alt='diowallet logo'/>
-            <form className="flex flex-col justify-center gap-4 w-full text-2xl">
-                <Input type="text" placeholder="Full name"/>
-                <Input type="email" placeholder="Email"/>
-                <Input type="password" placeholder="Password"/>
-                <Input type="password" placeholder="Confirm Password"/>
+            <form onSubmit={handleSubmit(handleSubmitForm)} className="flex flex-col justify-center gap-4 w-full text-2xl">
+                <Input type="text" placeholder="Full name" register={register} name="nome"/>
+                <Input type="email" placeholder="Email" register={register} name="email" />
+                <Input type="password" placeholder="Password" register={register} name="password"/>
+                <Input type="password" placeholder="Confirm Password" register={register} name="confirmPassword"/>
                 <Button type="submit" text="Signup"/>
             </form>
         </div>
